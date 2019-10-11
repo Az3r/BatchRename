76451d2d1ca34e400418d5abce2b7ed7a00dbfe5
Az3r <tuan99kghd@gmail.com>
2019-10-11 14:04:59 +0700

    Add project files.

---
+++ Batch-rename/Executor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Batch_rename
+{
+    class Executor
+    {
+        public static FileDescriptorModel Exeucute(FileDescriptorModel target, Func<FileDescriptorModel, FileDescriptorModel> method) { return method.Invoke(target); }
+    }
+}
