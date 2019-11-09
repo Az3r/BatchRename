0a72e44391dde521d5a7f641ca400ea2512e408d
Az3r <tuan99kghd@gmail.com>
2019-11-09 18:13:18 +0700

    + Add open explorer

--- Batch-rename/Views/Index.xaml.cs
+++ Batch-rename/Views/Index.xaml.cs
@@ -15,20 +15,33 @@ namespace BatchRename.Views
             Style = FindResource(typeof(Window)) as Style;
             Application.Current.MainWindow = this;
             InitializeComponent();
+            CreateFile(Path.Combine(System.Environment.CurrentDirectory, "Container"), 1000);
         }
 
         private void CreateFile(string directory, int n)
         {
-            if (Directory.Exists(directory))
+            if (!Directory.Exists(directory)) Directory.CreateDirectory(directory);
+            try
             {
                 Parallel.For(0, n, (i) =>
                 {
                     using (File.Create($"{directory}/{System.IO.Path.GetRandomFileName()}")) { }
                 });
             }
-            else throw new DirectoryNotFoundException($"{directory}");
+            catch (System.Exception e)
+            {
+                MessageBox.Show($"Unable to create test files\nError Message: {e.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
 
         public ItemViewModel ViewModel { get; set; } = new ItemViewModel();
+
+        private void OnOpenExplorer(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            if (e.Key == System.Windows.Input.Key.O && System.Windows.Input.Keyboard.IsKeyDown(System.Windows.Input.Key.LeftCtrl))
+            {
+                System.Diagnostics.Process.Start("explorer.exe",System.Environment.CurrentDirectory);
+            }
+        }
     }
 }
